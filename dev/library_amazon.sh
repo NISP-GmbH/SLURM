f238d80b591128ef084416ec794c2f37a3a9520b
Francisco Panis Kaseker <franciscopk@ni-sp.com>
2024-08-19 16:29:17 +0200

    specific HOME=/root to guarantee rpmbuild path

--- dev/library_amazon.sh
+++ dev/library_amazon.sh
@@ -44,7 +44,7 @@ installMariaDBforAmazon()
 {
     if [ "$slurm_accounting_support" == "1" ]
     then
-        if ! rpm -qa | egrep -iq "mariadb105-server "
+        if ! sudo rpm -qa | egrep -iq "mariadb105-server "
         then
             # SLURM accounting support
             if [ "$OSVERSION" == "2023" ]
@@ -148,7 +148,7 @@ echo completed.
 exit 0
 EOF
 
-    chmod +x /usr/sbin/create-munge-key
+    sudo chmod +x /usr/sbin/create-munge-key
 }
 
 setupMungeForAmazon()
@@ -186,7 +186,7 @@ buildSlurmForAmazon()
 
     if [[ "${OSVERSION}" == "2023" ]]
     then
-        cat << EOF | sudo tee dummy-mariadb-devel.spec
+        cat << EOF | sudo tee /root/dummy-mariadb-devel.spec
 Name:           dummy-mariadb-devel
 Version:        5.0.0
 Release:        1%{?dist}
@@ -207,7 +207,7 @@ This is a dummy package to satisfy the mysql-devel dependency.
 - Initial dummy package
 EOF
 
-        cat << EOF | sudo tee dummy-mysql-devel.spec
+        cat << EOF | sudo tee /root/dummy-mysql-devel.spec
 Name:           dummy-mysql-devel
 Version:        5.0.0
 Release:        1%{?dist}
@@ -227,25 +227,22 @@ This is a dummy package to satisfy the mysql-devel dependency.
 * Wed Aug 16 2023 Your Name <your.email@example.com> - 5.0.0-1
 - Initial dummy package
 EOF
-        sudo mkdir -p ~/rpmbuild/RPMS/noarch/
-        sudo rpmbuild -bb dummy-mysql-devel.spec
-        sudo rpmbuild -bb dummy-mariadb-devel.spec
-        sudo rpm -ivh ~/rpmbuild/RPMS/noarch/dummy-mysql-devel-5.0.0-1.amzn2023.noarch.rpm
-        sudo rpm -ivh ~/rpmbuild/RPMS/noarch/dummy-mariadb-devel-5.0.0-1.amzn2023.noarch.rpm
+        sudo env HOME=/root mkdir -p ~/rpmbuild/RPMS/noarch/
+        sudo env HOME=/root rpmbuild -bb /root/dummy-mysql-devel.spec
+        sudo env HOME=/root rpmbuild -bb /root/dummy-mariadb-devel.spec
+        sudo env HOME=/root rpm -ivh ~/rpmbuild/RPMS/noarch/dummy-mysql-devel-5.0.0-1.amzn2023.noarch.rpm
+        sudo env HOME=/root rpm -ivh ~/rpmbuild/RPMS/noarch/dummy-mariadb-devel-5.0.0-1.amzn2023.noarch.rpm
     fi
     
-    rpmbuild -ta slurm-${VER}.tar.bz2 --with mysql
+    sudo env HOME=/root rpmbuild -ta slurm-${VER}.tar.bz2 --with mysql
     rm slurm-${VER}.tar.bz2
     cd ..
-    rmdir slurm-tmp
+    sudo rm -rf slurm-tmp
 }
 
 setupSlurmForAmazon()
 {
-    cd ~/rpmbuild/RPMS/x86_64/
-
-    # skipping slurm-openlava and slurm-torque because of missing perl-Switch
-    sudo yum --nogpgcheck localinstall slurm-[0-9]*.amzn2023.x86_64.rpm slurm-contribs-*.amzn2023.x86_64.rpm slurm-devel-*.amzn2023.x86_64.rpm slurm-example-configs-*.amzn2023.x86_64.rpm slurm-libpmi-*.amzn2023.x86_64.rpm slurm-pam_slurm-*.amzn2023.x86_64.rpm slurm-perlapi-*.amzn2023.x86_64.rpm slurm-slurmctld-*.amzn2023.x86_64.rpm slurm-slurmd-*.amzn2023.x86_64.rpm slurm-slurmdbd-*.amzn2023.x86_64.rpm -y
+    sudo sh -c 'cd ~/rpmbuild/RPMS/x86_64/ && yum --nogpgcheck localinstall slurm-[0-9]*.amzn2023.x86_64.rpm slurm-contribs-*.amzn2023.x86_64.rpm slurm-devel-*.amzn2023.x86_64.rpm slurm-example-configs-*.amzn2023.x86_64.rpm slurm-libpmi-*.amzn2023.x86_64.rpm slurm-pam_slurm-*.amzn2023.x86_64.rpm slurm-perlapi-*.amzn2023.x86_64.rpm slurm-slurmctld-*.amzn2023.x86_64.rpm slurm-slurmd-*.amzn2023.x86_64.rpm slurm-slurmdbd-*.amzn2023.x86_64.rpm -y'
 
     # create the SLURM default configuration with
     # compute nodes called "NodeName=linux[1-32]"
