09b75f4149347d1dbed534dcee09d3fcaf74c2c5
Francisco Panis Kaseker <franciscopk@ni-sp.com>
2025-01-30 17:20:13 +0100

    add el8 packages

--- slurm_install.sh
+++ slurm_install.sh
@@ -600,7 +600,7 @@ buildSlurmForRedHatBased()
 	fi
 	if [ "$OSVERSION" == "8" ]
 	then
-	    sudo yum install rpm-build make -y
+	    sudo yum install rpm-build make autoconf automake dbus-devel -y
         if $ISOSREDHAT
         then
             sudo dnf config-manager --set-enabled codeready-builder-for-rhel-${OSVERSION}-x86_64-rpms
